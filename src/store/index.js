f0f2a97c08062c7defc3e2219d75d6f20c7cf7ab
bodek98 <bodzio.entire@gmail.com>
2023-01-26 16:34:37 +0100

    Added persisted data with vuex persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
   state: {
@@ -19,5 +20,5 @@ export default createStore({
       commit("ADD_FAVCOIN", favCoin);
     },
   },
-  modules: {},
+  plugins: [createPersistedState()],
 });
